5da897a85a256d8675bdb16aa674613bc26c51cd
Arham Saqib <arhamsaqib@outlook.com>
2021-11-03 20:04:26 +0500

    Turned all guards off in models

--- app/Models/BasicInformation.php
+++ app/Models/BasicInformation.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class BasicInformation extends Model
 {
     use HasFactory;
+    protected $guarded = [];
 }
--- app/Models/Words.php
+++ app/Models/Words.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Words extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
 }
