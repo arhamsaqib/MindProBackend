357a9d4e93ed7974bae329668048c685c31ed775
Arham Saqib <arhamsaqib@outlook.com>
2021-11-03 19:56:17 +0500

    Added migrations & models for judges, contestants, basicinformation

---
+++ database/migrations/2021_11_03_144801_create_basic_information_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBasicInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('basic_information', function (Blueprint $table) {
+            $table->id();
+            $table->string('uid')->unique();
+            $table->string('fname')->nullable();
+            $table->string('lname')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('basic_information');
+    }
+}
