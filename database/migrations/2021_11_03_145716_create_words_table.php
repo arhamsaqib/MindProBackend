10d020726f281d7f32a39acd85368b3ab1117a43
Arham Saqib <arhamsaqib@outlook.com>
2021-11-03 20:03:12 +0500

    Added migrations & models for attempts, labels, words

---
+++ database/migrations/2021_11_03_145716_create_words_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+            $table->string('judge_id')->nullable();
+            $table->string('hint1')->nullable();
+            $table->string('hint2')->nullable();
+            $table->string('hint3')->nullable();
+            $table->string('category')->nullable();
+            $table->string('word')->nullable();
+            $table->string('time_allowed')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
---
+++ database/migrations/2021_11_03_145853_create_contestant_attempts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContestantAttemptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contestant_attempts', function (Blueprint $table) {
+            $table->id();
+            $table->string('uid')->nullable();
+            $table->string('word_id')->nullable();
+            $table->string('time_taken')->nullable();
+            $table->string('score')->nullable();
+            $table->string('hint1_status')->nullable();
+            $table->string('hint2_status')->nullable();
+            $table->string('hint3_status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contestant_attempts');
+    }
+}
