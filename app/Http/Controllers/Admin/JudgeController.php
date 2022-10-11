1001855b59f792d0c45c714f07db28b85577b529
iamshujasaqib <iamshujasaqib@gmail.com>
2022-10-11 15:18:06 +0500

    admin route setup

---
+++ app/Http/Controllers/Admin/JudgeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Models\Judges;
+use App\Http\Controllers\Controller;
+
+class JudgeController extends Controller
+{
+    public function index(){
+        return 'Hello';
+    }
+}
